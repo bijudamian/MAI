dcb19bf5a75d55027f0f486f75b5c4c452bf863a
Biju Damian <96348059+bijudamian@users.noreply.github.com>
2025-09-08 12:25:25 +0530

    Delete config directory

--- config/index.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  v0ApiKey: process.env.V0_API_KEY || '',
-  claudeApiKey: process.env.CLAUDE_API_KEY || '',
-  openAIApiKey: process.env.OPENAI_API_KEY || '',
-  boltAIApiKey: process.env.BOLT_AI_API_KEY || '',
-}
-
